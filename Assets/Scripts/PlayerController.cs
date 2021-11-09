6bbb5d74792f8c053a1e6eb85ae75dbdaf5c4cbe
Joel Stokes <jestokes@odu.edu>
2021-11-09 16:46:08 -0500

    Player ground checks for jump handling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,18 +25,22 @@ public class PlayerController : MonoBehaviour
     private float forwardSpeed = 5f;
     private float forwardInc = .05f;   //Speed to add every point increase
 
+    private int layerMask;
+
     // Start is called before the first frame update
     void Start()
     {
         rigi = GetComponent<Rigidbody2D>();
 
         distToGround = GetComponent<Collider2D>().bounds.extents.y;
+
+        layerMask = LayerMask.GetMask("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!slamming){
+        if (!slamming && !AgainstWall()){
             transform.position = new Vector3(
                 transform.position.x + ((forwardSpeed + (forwardInc * score)) * Time.deltaTime), transform.position.y, transform.position.z);
         }
@@ -52,17 +56,17 @@ public class PlayerController : MonoBehaviour
                 rigi.AddForce(Vector2.up * jumpPower);
             } else if (jumping) //Hold jump to get higher
             {
-                rigi.AddForce(Vector2.up * jumpSubtractHeld);
+                rigi.AddForce(Vector2.up * (jumpSubtractHeld * Time.deltaTime));
             } else if (!slamming)   //Slam down from the air. Prevent multi-slam from multi-clicks
             {
-                rigi.AddForce(Vector2.up * slamSpeed);
+                rigi.AddForce(Vector2.up * (slamSpeed * Time.deltaTime));
                 slamming = true;
             }
         } else
         {
             if (!IsGrounded())  //Subtract from jump until maxFall speed
             {
-                rigi.AddForce(Vector2.up * jumpSubtractRelease);
+                rigi.AddForce(Vector2.up * (jumpSubtractRelease * Time.deltaTime));
             }
         }
     }
@@ -73,7 +77,21 @@ public class PlayerController : MonoBehaviour
     }
 
     private bool IsGrounded(){
-        return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + 0.1f);
+        //Debug.DrawRay(transform.position, Vector2.down, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distToGround + .1f, layerMask);
+        return (hit.collider != null && hit.transform.tag == "Ground");  //Prevent jumping off collectibles or spikes
+    }
+
+    private bool AgainstWall(){
+        RaycastHit2D hit = (Physics2D.Raycast(transform.position, Vector2.right, distToGround + .1f, layerMask));
+        if (hit.collider != null && hit.transform.tag == "Ground")  //Prevent stopping at collectibles or spikes
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
