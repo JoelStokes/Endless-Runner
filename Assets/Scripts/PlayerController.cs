d4ccd394667214ccee3c606a58cd7f5a19c06436
Joel Stokes <jestokes@odu.edu>
2021-11-09 13:51:48 -0500

    Player movement logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,50 +4,65 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rigi;
+
     private bool slamming = false;
-    private bool grounded = false;
     private bool jumping = false;
+
+    private float distToGround;
 
     private int score = 0;
     private float scoreTimer = 1;   //How long to walk before 1 point added to score
 
     private float jumpHoldTimer = .4f;
 
-    private float jumpPower = 1;
-    private float jumpSubtractHeld = .1f;
-    private float jumpSubtractRelease = .2f;
-    private float maxFall = -2;
+    private float jumpPower = 100;
+    private float jumpSubtractHeld = -10f;
+    private float jumpSubtractRelease = -20f;
+    private float maxFall = -20;
+    private float slamSpeed = -50f;
 
-    private float forwardSpeed = .01f;
+    private float forwardSpeed = 5f;
+    private float forwardInc = .05f;   //Speed to add every point increase
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigi = GetComponent<Rigidbody2D>();
+
+        distToGround = GetComponent<Collider2D>().bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + forwardSpeed, transform.position.y, transform.position.z);
+        if (!slamming){
+            transform.position = new Vector3(
+                transform.position.x + ((forwardSpeed + (forwardInc * score)) * Time.deltaTime), transform.position.y, transform.position.z);
+        }
+
+        if (slamming && IsGrounded()){
+            slamming = false;
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (grounded)   //Start jump off ground
+            if (IsGrounded())   //Start jump off ground
             {
-
+                rigi.AddForce(Vector2.up * jumpPower);
             } else if (jumping) //Hold jump to get higher
             {
-
+                rigi.AddForce(Vector2.up * jumpSubtractHeld);
             } else if (!slamming)   //Slam down from the air. Prevent multi-slam from multi-clicks
             {
-
+                rigi.AddForce(Vector2.up * slamSpeed);
+                slamming = true;
             }
         } else
         {
-            if (!grounded)  //Subtract from jump until maxFall speed
+            if (!IsGrounded())  //Subtract from jump until maxFall speed
             {
-
+                rigi.AddForce(Vector2.up * jumpSubtractRelease);
             }
         }
     }
@@ -55,6 +70,10 @@ public class PlayerController : MonoBehaviour
     public void Die()
     {
         Destroy(gameObject);
+    }
+
+    private bool IsGrounded(){
+        return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + 0.1f);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
